114603cf7c89d7c4187d3327d0334e81a7483223
My Tien <ntm.tien.15@gmail.com>
2024-10-24 16:11:46 +0700

    fix bug

--- lib/tabScreens/swipping_screen.dart
+++ lib/tabScreens/swipping_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../controllers/profile_controller..dart';
+import '../controllers/profile_controller.dart';
 
 class SwippingScreen extends StatefulWidget
 {
@@ -12,15 +12,24 @@ class SwippingScreen extends StatefulWidget
 
 class _SwippingScreenState extends State<SwippingScreen> {
   ProfileController profileController = Get.put(ProfileController());
+
+  @override
+    void dispose() {
+      profileController.dispose();
+      super.dispose();
+    }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Obx(()
       {
+        print(profileController.allUsersProfileList.length);
+
         return PageView.builder(
           itemCount: profileController.allUsersProfileList.length,
-          controller: PageController(initialPage: 0, viewportFraction: 1),
+          controller: PageController(initialPage: 0, viewportFraction: 0.9),
+          physics: BouncingScrollPhysics(), 
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) 
           {
